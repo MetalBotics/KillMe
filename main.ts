e63718fd866fca60313c75b68b84a4d6c417e7b7
Enzo Gutiérrez Pereira <enzogp2005@gmail.com>
2025-09-18 21:27:25 -0300

    adjust for minutes or seconds and GET request, easy to do via browser

--- main.ts
+++ main.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import { exec } from 'child_process';
+import cors from 'cors';
 
 const app = express();
 const port = 5000;
+app.use(cors());
+app.use(express.json());
 
 // Simple API for turning the host PC off (Windows only)
 // Do not expose this, use only in internal networks
@@ -11,12 +14,23 @@ app.get('/', (req, res) => {
   res.send('KillMe API running!');
 });
 
-app.post('/kill', (req, res) => {
-  exec("shutdown -f -s -t 60")
-  res.json({ message: 'Kill command executed' }).status(200);
+app.get('/kill/:time', (req, res) => {
+  let time: string | Number = req.params.time;
+  if(time.endsWith("min")) {
+    time = parseInt(time.replace("min", ""));
+    time = time as number * 60;
+  }
+  if(!isNaN(time as number)) {
+    res.json({ message: `Kill command executed, PC will shut down in ${time} seconds` }).status(200);
+    exec(`shutdown -f -s -t ${time}`)
+  }
+  else {
+    res.json({ message: 'Invalid time parameter' }).status(400);
+  }
+  console.log(time)
 });
 
-app.post('/mercy', (req, res) => {
+app.get('/mercy', (req, res) => {
   exec("shutdown -a")
   res.json({ message: 'Mercy command executed' }).status(200);
 });
